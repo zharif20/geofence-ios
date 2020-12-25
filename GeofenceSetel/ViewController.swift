0a5f9e38000d284baf8461340753800e1a5c4825
Zharif Hadi <mzmhadi@gmail.com>
2020-12-25 23:56:09 +0800

    Readded files

---
+++ GeofenceSetel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceSetel
+//
+//  Created by Zharif Hadi  on 25/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
